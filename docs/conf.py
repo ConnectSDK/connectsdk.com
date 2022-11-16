29ee56efd0b6ff12d7f8e7c749fc2538f26e1b89
dbkim77 <daebum.kim@lge.com>
2022-11-16 11:44:41 +0900

    copyright 수정

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ from recommonmark.transform import AutoStructify
 
 project = 'connectSDK'
 author = 'LG Electronics'
-copyright = 'Copyright © 2022 LG Electronics. All Rights Reserved.'
+copyright = '2022 LG Electronics. All Rights Reserved.'
 
 
 # -- General configuration ---------------------------------------------------
